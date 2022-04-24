f629f5c49906ea0254318ac83ba9c43c7d703ff7
srijanya yarram <100142338+Syar123@users.noreply.github.com>
2022-04-23 20:12:06 -0500

    additional changes to read me

--- sql-example.sql
+++ sql-example.sql
@@ -1,6 +1,20 @@
-SELECT 
- EXTRACT(YEAR from starttime) as year,  
-  COUNT(DISTINCT(bikeid)) as num_bikes
-FROM `bigquery-public-data.new_york.citibike_trips`
-GROUP BY year
-ORDER BY year
+WITH cases_by_country AS (  
+SELECT    
+country_name AS country,    
+sum(cumulative_confirmed) AS cases,   
+sum(cumulative_recovered) AS recovered_cases  
+FROM    bigquery_public_data.covid19_open_data.covid19_open_data  WHERE    
+date = '2020-03-25'  
+GROUP BY    
+country_name ), recovered_rate AS 
+(SELECT  country, cases, recovered_cases,  (recovered_cases * 100)/cases AS recovery_rate
+FROM cases_by_country)SELECT 
+country, 
+cases AS confirmed_cases, 
+recovered_cases, 
+recovery_rate
+FROM 
+recovered_rate
+WHERE cases > 50000
+ORDER BY recovery_rate desc
+LIMIT 10
